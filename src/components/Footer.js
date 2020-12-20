df39d783f7e778ccef9da4a00eb3d6d9be7822b5
Jacob Norwood <jacobnorwood92@gmail.com>
2020-12-20 03:52:40 -0600

    Accessablity edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,28 +7,28 @@ class Footer extends React.Component {
                 <div className="inner">
                     <ul className="labeled-icons">
                         <li>
-                            <h3 className="icon fa-home"><span className="label">Address</span></h3>
+                            <span className="icon fa-home h3"><span className="label">Address</span></span>
                             Kansas City, Mo<br />
                             United States
                         </li>
                         <li>
-                            <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
+                            <span className="icon fa-envelope-o h3"><span className="label">Email</span></span>
                             <a href="mailto:jacobnorwood92@gmail.com">jacobnorwood92@gmail.com</a>
                         </li>
                     </ul>
                     <ul className="icons">
                         <li>
-                        <a href="https://www.linkedin.com/in/jwnorwood/" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.linkedin.com/in/jwnorwood/" title="Linkedin" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer">
                             <span className="label">Linkedin</span>
                         </a>
                         </li>
                         <li>
-                        <a href="https://github.com/jwnorwood" className="icon fa-github" target="_blank" rel="noopener noreferrer">
+                        <a href="https://github.com/jwnorwood" title="Github" className="icon fa-github" target="_blank" rel="noopener noreferrer">
                             <span className="label">Github</span>
                         </a>
                         </li>
                         <li>
-                        <a href="https://www.behance.net/jacobnorwood3" className="icon fa-behance" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.behance.net/jacobnorwood3" title="Behance" className="icon fa-behance" target="_blank" rel="noopener noreferrer">
                             <span className="label">Behance</span>
                         </a>
                         </li>
